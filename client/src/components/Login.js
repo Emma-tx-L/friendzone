65dfb41e67f8c08997598a598a555f22b97d671e
John Aguilar <42220961+johnagl@users.noreply.github.com>
2020-03-15 13:35:30 -0700

    Login front end

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,69 @@
 import React from 'react';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Container from '@material-ui/core/Container';
 
-function Login() {
-  return (
-    <h1>Login</h1>
-  );
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value });
+  }
+
+  render() {
+    console.log(this.state.email);
+    console.log(this.state.password);
+    return (
+      <Container component="main" maxWidth="xs">
+        <form noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={this.handleEmailChange}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={this.handlePasswordChange}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="default"
+          >
+            Sign In
+          </Button>
+        </form>
+      </Container>
+    );
+  }
 }
 
 export default Login;
