fb8fbfd162327efb1a1f9733b562e1ab5a6493d5
John Aguilar <42220961+johnagl@users.noreply.github.com>
2020-03-22 17:57:25 -0700

    Display rest of event information in events page

--- client/src/components/EventPage.js
+++ client/src/components/EventPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from "axios";
 class Event extends React.Component {
   state = {
-    name: '',
+    event: null,
   }
 
   async componentDidMount() {
@@ -10,10 +10,12 @@ class Event extends React.Component {
     const id = pathArr[2];
       try{
         const res = await axios.get("http://localhost:5000/api/event/my-events/" + id);
+        console.log('res: ' + JSON.stringify(res));
         if (res && res.status == 200){
           const event = res.data[0];
-          const name = event.name;
-          this.setState({name: name});
+          this.setState({
+            event: event,
+          });
         } else {
           this.setState({name: 'unable to find'})
         }
@@ -23,13 +25,24 @@ class Event extends React.Component {
   }
 
   render() {
-    //TODO Display all the other event information besides name
     //TODO: Use Moment for dates.
     //TODO: Style
-    const name = this.state.name && this.state.name
+    const name = this.state.event?.name;
+    const starttime = this.state.event?.starttime;
+    const endtime = this.state.event?.endtime;
+    const description = this.state.event?.description;
+    const activitytype = this.state.event?.activitytype;
+    const activitylevel = this.state.event?.activitylevel;
+    const streetname = this.state.event?.streetname;
+    const streetnumber = this.state.event?.streetnumber;
+    const postalcode = this.state.event?.postalcode;
     return (
         <React.Fragment>
           <h1>{name}</h1>
+          <h3>{description}</h3>
+          <h3>Type: {activitytype} Level: {activitylevel}</h3>
+          <h3>Start: {starttime} End: {endtime}</h3>
+          <h3>Location: {streetnumber} {streetname} {postalcode}</h3>
         </React.Fragment>
     );
   }
