905030ce5b42ce7929039f3e3d211832d5eec243
Emma Liu <gigitxl@yahoo.ca>
2020-03-29 13:19:59 -0700

    complete categories home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -11,26 +11,32 @@ class Home extends React.Component {
     super(props);
     this.state = {
     };
-    URL = `/src/assets/categories_fitness.png`;
-    // URL='https://pbs.twimg.com/media/EUTTGIZUMAAUQ2W?format=png&name=small';
+    const testURL = `/src/assets/categories_fitness.png`;
+    // sorry, hack to get images showing, these have been uploaded to a private twitter account
+    // - Emma
+    const fitnessURL = 'https://pbs.twimg.com/media/EUTTGIZUMAAUQ2W?format=png&name=small';
+    const techURL = 'https://pbs.twimg.com/media/EUTXv-yU4AAy6cp?format=png&name=small';
+    const musicURL = 'https://pbs.twimg.com/media/EUTXv-wU0AApjWW?format=png&name=small';
+    const artsURL = 'https://pbs.twimg.com/media/EUTXv-vUMAE3VpT?format=png&name=small';
+    const foodndrinkURL = 'https://pbs.twimg.com/media/EUTXv-wUEAARxg3?format=png&name=small';
     this.activityTypes = [
-      {type: 'Fitness', img: URL},
-      {type: 'Technology', img: URL},
-      {type: 'Music', img: URL},
-      {type: 'Arts', img: URL},
-      {type: 'Food & Drink', img: URL}
+      {type: 'Fitness', img: fitnessURL},
+      {type: 'Technology', img: techURL},
+      {type: 'Music', img: musicURL},
+      {type: 'Arts', img: artsURL},
+      {type: 'Food & Drink', img: foodndrinkURL}
     ]
   }
   render() {
     return (
       <Container>
-        <Container maxWidth="md" style={{position: 'relative', backgroundColor: this.colour, height: '15vh'}}>
-          <Typography variant="h3" style={{ position: 'absolute', fontWeight:'bold',letterSpacing:'0.05em', top: '65%', left: '3vh', transform: 'translateY(-50%)'}}>
+        <Container maxWidth="md" style={{position: 'relative', height: '25vh'}}>
+          <Typography variant="h3" style={{ position: 'absolute', fontWeight:'bold',letterSpacing:'0.05em', top: '40%', left: '3vh', transform: 'translateY(-50%)'}}>
             HOME
           </Typography>
         </Container>
-        <Container maxWidth="md" style={{backgroundColor: this.props.colour }}>
-          <GridList cellHeight="auto" className="category-card-list" cols={3} spacing={10}>
+        <Container maxWidth="md">
+          <GridList cellHeight="auto" className="category-card-list" cols={2} spacing={30}>
           {this.activityTypes.map(category => (
               <GridListTile cols={1} style={{ height: 'auto' }} key={category.type}>
                   <CategoryCard type={category.type} image={category.img}/>
